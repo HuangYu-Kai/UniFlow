764afbd0765184a53de0dab2c02cfd142b8ce04c
XuanOuO <deanchou8009@gmail.com>
2026-03-03 23:26:25 +0800

    git

--- mobile_app/lib/services/signaling.dart
+++ mobile_app/lib/services/signaling.dart
@@ -17,7 +17,7 @@ typedef void CallRequestCallback(String roomId, String senderId);
 typedef void CallAcceptedCallback(String accepterId);
 
 class Signaling {
-  final String _socketUrl = 'https://50ef-61-65-116-7.ngrok-free.app'; // 請確認 IP
+  final String _socketUrl = 'https://50ef-61-65-116-7.ngrok-free.app'; // 容我先自己使用，感恩的心
   static const platform = MethodChannel('com.example.app/bring_to_front');
 
   IO.Socket? socket;
