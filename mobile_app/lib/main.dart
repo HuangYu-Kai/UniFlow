068b0621794fbd59fdd8b0413cfbf7ef3b2636ae
XuanOuO <deanchou8009@gmail.com>
2026-03-05 22:35:37 +0800

    git

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -13,7 +13,7 @@ final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 @pragma('vm:entry-point')
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   // If you're going to use other Firebase services in the background, such as Firestore,
-  // make sure you call `initializeApp` before using other Firebase services.
+  // make sure you call `initializeApp` before using other Firebase services
   await Firebase.initializeApp();
   print("Handling a background message: \${message.messageId}");
 
